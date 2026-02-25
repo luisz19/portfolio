d05ac3753bb2e7874102eabcb9b57072c6338c74
Luis Henrique <luiss18.code@gmail.com>
2026-02-24 21:45:01 -0300

    style: add h-screen class to Hero section for full viewport height

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,8 +10,7 @@ const iconMap = {
 function Hero () {
 
     return (
-        <section className="py-section flex justify-center flex-col gap-7">
-            
+        <section className="py-section flex justify-center flex-col gap-7 h-screen">
             <h1 className="text-7xl font-bold  block text-white/70">
                 {HERO_CONTENT.title}
                 <span className="text-white"><br/>{HERO_CONTENT.subtitle}</span>
